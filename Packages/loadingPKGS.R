ad2b3582a9053016c665462b35d59d62c022ae19
pdasg <purnima.dasgupta27@gmail.com>
2016-01-10 10:28:50 +0100

    done

---
+++ Packages/loadingPKGS.R
@@ -0,0 +1,15 @@
+####################################################
+###########loading packages#########################
+####################################################
+
+require(DMwR)
+require(gdata)
+require(gstat)
+require(gtools)
+require(sp)
+require(spdep)
+require(moments)
+require(MASS)
+require(fitdistrplus)
+require(logspline)
+require(qualityTools)
